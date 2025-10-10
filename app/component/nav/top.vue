ebf0349b2ed56fc2e0a7717a9e25db35266834ec
benni <benni.huff@gmail.com>
2025-10-10 13:30:55 +0200

    button klick on sub menu hover

--- app/component/nav/top.vue
+++ app/component/nav/top.vue
@@ -16,10 +16,14 @@ import {ref} from 'vue'
 
 const toggleState = ref(true)
 
+function test() {
+  alert("hoii")
+}
+
 </script>
 
 <template>
-  <DropdownMenuRoot v-model:open="toggleState">
+  <DropdownMenuRoot :open="true">
     <DropdownMenuTrigger>
       test
     </DropdownMenuTrigger>
@@ -28,7 +32,7 @@ const toggleState = ref(true)
       <DropdownMenuContent class="bg-white border border-gray-200 rounded-xl shadow-lg py-2 min-w-[200px] text-sm">
         <DropdownMenuSub v-for="category in data.categories" :key="category.id">
           <DropdownMenuSubTrigger
-              class="px-4 py-2 text-gray-700 cursor-pointer hover:bg-gray-100 transition-colors"
+              class="px-4 py-2 text-gray-700 cursor-pointer hover:bg-gray-100 transition-colors" @click="test"
               value="inner1">
             {{ category.title }}
           </DropdownMenuSubTrigger>
@@ -39,14 +43,14 @@ const toggleState = ref(true)
               <DropdownMenuSub v-for="sub in category.sub_categories" :key="sub.id">
                 <DropdownMenuSubTrigger
                     class="px-4 py-2 text-gray-700 cursor-pointer hover:bg-gray-100 transition-colors"
-                    value="inner1">
+                    value="inner1" @click="test">
                   {{ sub.title }}
                 </DropdownMenuSubTrigger>
 
                 <DropdownMenuPortal>
                   <DropdownMenuSubContent
                       class="bg-white border border-gray-200 rounded-xl shadow-lg py-2 min-w-[200px] text-sm">
-                    <DropdownMenuItem v-for="topic in sub.topics" :key="topic"
+                    <DropdownMenuItem @select="test" v-for="topic in sub.topics" :key="topic"
                                       class="px-4 py-2 text-gray-700 cursor-pointer hover:bg-gray-100 transition-colors">
                       {{ topic }}
                     </DropdownMenuItem>
