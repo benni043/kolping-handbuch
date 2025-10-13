7c6eb4bea8f90d9b270ae1a18a2039f5b913bdc4
benni <benni.huff@gmail.com>
2025-10-13 08:50:39 +0200

    added img

--- app/component/nav/top.vue
+++ app/component/nav/top.vue
@@ -23,46 +23,50 @@ function test() {
 </script>
 
 <template>
-  <DropdownMenuRoot :open="true">
-    <DropdownMenuTrigger>
-      test
-    </DropdownMenuTrigger>
+  <div class="flex justify-center items-start mt-5">
+    <DropdownMenuRoot :open="true">
+      <DropdownMenuTrigger>
+        test
+      </DropdownMenuTrigger>
 
-    <DropdownMenuPortal>
-      <DropdownMenuContent class="bg-white border border-gray-200 rounded-xl shadow-lg py-2 min-w-[200px] text-sm">
-        <DropdownMenuSub v-for="category in data.categories" :key="category.id">
-          <DropdownMenuSubTrigger
-              class="px-4 py-2 text-gray-700 cursor-pointer hover:bg-gray-100 transition-colors" @click="test"
-              value="inner1">
-            {{ category.title }}
-          </DropdownMenuSubTrigger>
+      <DropdownMenuPortal>
+        <DropdownMenuContent class="bg-white border border-gray-200 rounded-xl shadow-lg py-2 min-w-[200px] text-sm">
+          <DropdownMenuSub v-for="category in data.categories" :key="category.id">
+            <DropdownMenuSubTrigger
+                class="px-4 py-2 text-gray-700 cursor-pointer hover:bg-gray-100 transition-colors" @click="test"
+                value="inner1">
+              {{ category.title }}
+            </DropdownMenuSubTrigger>
 
-          <DropdownMenuPortal>
-            <DropdownMenuSubContent
-                class="bg-white border border-gray-200 rounded-xl shadow-lg py-2 min-w-[200px] text-sm">
-              <DropdownMenuSub v-for="sub in category.sub_categories" :key="sub.id">
-                <DropdownMenuSubTrigger
-                    class="px-4 py-2 text-gray-700 cursor-pointer hover:bg-gray-100 transition-colors"
-                    value="inner1" @click="test">
-                  {{ sub.title }}
-                </DropdownMenuSubTrigger>
+            <DropdownMenuPortal>
+              <DropdownMenuSubContent
+                  class="bg-white border border-gray-200 rounded-xl shadow-lg py-2 min-w-[200px] text-sm">
+                <DropdownMenuSub v-for="sub in category.sub_categories" :key="sub.id">
+                  <DropdownMenuSubTrigger
+                      class="px-4 py-2 text-gray-700 cursor-pointer hover:bg-gray-100 transition-colors"
+                      value="inner1" @click="test">
+                    {{ sub.title }}
+                  </DropdownMenuSubTrigger>
 
-                <DropdownMenuPortal>
-                  <DropdownMenuSubContent
-                      class="bg-white border border-gray-200 rounded-xl shadow-lg py-2 min-w-[200px] text-sm">
-                    <DropdownMenuItem @select="test" v-for="topic in sub.topics" :key="topic"
-                                      class="px-4 py-2 text-gray-700 cursor-pointer hover:bg-gray-100 transition-colors">
-                      {{ topic }}
-                    </DropdownMenuItem>
-                  </DropdownMenuSubContent>
-                </DropdownMenuPortal>
-              </DropdownMenuSub>
-            </DropdownMenuSubContent>
-          </DropdownMenuPortal>
-        </DropdownMenuSub>
-      </DropdownMenuContent>
-    </DropdownMenuPortal>
-  </DropdownMenuRoot>
+                  <DropdownMenuPortal>
+                    <DropdownMenuSubContent
+                        class="bg-white border border-gray-200 rounded-xl shadow-lg py-2 min-w-[200px] text-sm">
+                      <DropdownMenuItem @select="test" v-for="topic in sub.topics" :key="topic"
+                                        class="px-4 py-2 text-gray-700 cursor-pointer hover:bg-gray-100 transition-colors">
+                        {{ topic }}
+                      </DropdownMenuItem>
+                    </DropdownMenuSubContent>
+                  </DropdownMenuPortal>
+                </DropdownMenuSub>
+              </DropdownMenuSubContent>
+            </DropdownMenuPortal>
+          </DropdownMenuSub>
+        </DropdownMenuContent>
+      </DropdownMenuPortal>
+    </DropdownMenuRoot>
+
+    <img src="/img/header.png" alt="header">
+  </div>
 </template>
 
 <style scoped>
