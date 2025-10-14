c92056d11b801aab43911513855acb660f255bdd
benni <benni.huff@gmail.com>
2025-10-14 10:21:34 +0200

    added schritt to form

--- app/component/kernprozess-creator.vue
+++ app/component/kernprozess-creator.vue
@@ -12,66 +12,79 @@ let verantwortlicherOrange: Ref<string> = ref("");
 let informationOrange: Ref<string> = ref("");
 
 
-const vorgaben = ref<{ text: string; link?: string; hasLink: boolean }[]>([
+const vorgabenBlue = ref<{ text: string; link?: string; hasLink: boolean }[]>([
   {text: "", hasLink: false, link: ""}
 ]);
 
-function addItem() {
-  vorgaben.value.push({text: "", hasLink: false, link: ""});
+function addVorgabeBlue() {
+  vorgabenBlue.value.push({text: "", hasLink: false, link: ""});
 }
 
-function removeItem(index: number) {
-  vorgaben.value.splice(index, 1);
+function removeVorgabeBlue(index: number) {
+  vorgabenBlue.value.splice(index, 1);
 }
 
-function send() {
-  console.log(vorgaben.value);
+function postForm() {
+  console.log(vorgabenBlue.value);
 }
 </script>
 
 <template>
   <UCard class="max-w-xl mx-auto mt-10">
     <UForm class="space-y-5">
-      <h1>Vorgaben / Arbeitshilfen</h1>
-
-      <div v-for="(item, index) in vorgaben" :key="index" class="border p-4 rounded-xl space-y-3 relative">
-        <UInput
-            v-model="item.text"
-            placeholder="Vorgaben / Arbeitshilfen..."
-            icon="i-heroicons-document-text"
-        />
-
-        <UCheckbox v-model="item.hasLink" label="Als Link hinzufügen"/>
+      <UCard>
+        <h1 class="mb-2">Schritt</h1>
 
         <UInput
-            v-if="item.hasLink"
-            v-model="item.link"
-            placeholder="https://..."
-            icon="i-heroicons-link"
+            v-model="schrittCount"
+            placeholder="Schritt"
+            type="number"
+            icon="i-heroicons-document-plus"
         />
+      </UCard>
+
+      <UCard>
+        <h1 class="mb-2">Vorgaben / Arbeitshilfen</h1>
+
+        <div v-for="(item, index) in vorgabenBlue" :key="index" class="mb-8 space-y-3 relative">
+          <UInput
+              v-model="item.text"
+              placeholder="Vorgaben / Arbeitshilfen..."
+              icon="i-heroicons-document-text"
+          />
+
+          <UCheckbox v-model="item.hasLink" label="Als Link hinzufügen"/>
+
+          <UInput
+              v-if="item.hasLink"
+              v-model="item.link"
+              placeholder="https://..."
+              icon="i-heroicons-link"
+          />
+
+          <UButton
+              color="error"
+              variant="soft"
+              icon="i-heroicons-trash"
+              class="absolute top-2 right-2"
+              @click="removeVorgabeBlue(index)"
+          />
+        </div>
 
         <UButton
-            color="error"
-            variant="soft"
-            icon="i-heroicons-trash"
-            class="absolute top-2 right-2"
-            @click="removeItem(index)"
-        />
-      </div>
-
-      <div class="flex justify-between mt-6">
-        <UButton
-            @click="addItem"
+            @click="addVorgabeBlue"
             color="neutral"
             variant="soft"
+            class="mt-2"
             icon="i-heroicons-plus"
         >
           Hinzufügen
         </UButton>
-      </div>
+      </UCard>
+
 
       <UButton
-          @click="send"
+          @click="postForm"
           color="primary"
           variant="solid"
           icon="i-heroicons-paper-airplane"
