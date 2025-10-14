91c03c74bc2f637e85f61dc3ea1e46bc36d92c69
benni <benni.huff@gmail.com>
2025-10-14 13:21:54 +0200

    changes

--- server/api/saveForm.post.ts
+++ server/api/saveForm.post.ts
@@ -3,7 +3,7 @@ import {join} from "path"
 
 export default defineEventHandler(async (event) => {
     const body = await readBody(event)
-    const filePath = join(process.cwd(), "data", `form_${Date.now()}.json`)
+    const filePath = join(process.cwd(), "data/test", `form_${Date.now()}.json`)
 
     await writeFile(filePath, JSON.stringify(body, null, 2), "utf-8")
 
