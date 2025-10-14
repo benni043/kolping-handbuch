a49db80ba4063d72da97af952dfcc0b400af3cd5
benni <benni.huff@gmail.com>
2025-10-14 11:13:20 +0200

    changes

--- app/component/nav/top.vue
+++ app/component/nav/top.vue
@@ -10,9 +10,20 @@ import {
   DropdownMenuTrigger,
 } from "reka-ui";
 
-const {data} = await useFetch("/api/files/metadata/metadata.json");
-
 import {ref} from "vue";
+
+const colorMode = useColorMode()
+
+const isDark = computed({
+  get() {
+    return colorMode.value === 'dark'
+  },
+  set(_isDark) {
+    colorMode.preference = _isDark ? 'dark' : 'light'
+  }
+})
+
+const {data} = await useFetch("/api/files/metadata/metadata.json");
 
 const toggleState = ref(true);
 
@@ -22,70 +33,84 @@ function test() {
 </script>
 
 <template>
-  <div class="flex justify-evenly items-start bg-red-200 h-80">
-    <DropdownMenuRoot :open="true">
-      <DropdownMenuTrigger class="flex-1 h-6"></DropdownMenuTrigger>
+  <ClientOnly v-if="!colorMode?.forced">
+    <UButton
+        :icon="isDark ? 'i-lucide-moon' : 'i-lucide-sun'"
+        color="neutral"
+        variant="ghost"
+        :aria-label="`Switch to ${isDark ? 'light' : 'dark'} mode`"
+        @click="isDark = !isDark"
+    />
 
-      <DropdownMenuPortal>
-        <DropdownMenuContent
-            class="bg-white border border-gray-200 shadow-lg py-2 min-w-[200px] text-sm"
-        >
-          <DropdownMenuSub
-              v-for="category in data.categories"
-              :key="category.id"
-          >
-            <DropdownMenuSubTrigger
-                class="px-4 py-2 text-gray-700 cursor-pointer hover:bg-gray-100 transition-colors"
-                @click="test"
-                value="inner1"
-            >
-              {{ category.title }}
-            </DropdownMenuSubTrigger>
+    <template #fallback>
+      <div class="size-8"/>
+    </template>
+  </ClientOnly>
 
-            <DropdownMenuPortal>
-              <DropdownMenuSubContent
-                  class="bg-white border border-gray-200 shadow-lg min-w-[200px] text-sm"
-              >
-                <DropdownMenuSub
-                    v-for="sub in category.sub_categories"
-                    :key="sub.id"
-                >
-                  <DropdownMenuSubTrigger
-                      class="px-4 py-2 text-gray-700 cursor-pointer hover:bg-gray-100 transition-colors"
-                      value="inner1"
-                      @click="test"
-                  >
-                    {{ sub.title }}
-                  </DropdownMenuSubTrigger>
+  <!--  <div class="flex justify-evenly items-start bg-blue-300 h-80">-->
+  <!--    <DropdownMenuRoot :open="true">-->
+  <!--      <DropdownMenuTrigger class="flex-1 h-6"></DropdownMenuTrigger>-->
 
-                  <DropdownMenuPortal>
-                    <DropdownMenuSubContent
-                        class="bg-white border border-gray-200 shadow-lg min-w-[200px] text-sm"
-                    >
-                      <DropdownMenuItem
-                          @select="test"
-                          v-for="topic in sub.topics"
-                          :key="topic"
-                          class="px-4 py-2 text-gray-700 cursor-pointer hover:bg-gray-100 transition-colors"
-                      >
-                        {{ topic }}
-                      </DropdownMenuItem>
-                    </DropdownMenuSubContent>
-                  </DropdownMenuPortal>
-                </DropdownMenuSub>
-              </DropdownMenuSubContent>
-            </DropdownMenuPortal>
-          </DropdownMenuSub>
-        </DropdownMenuContent>
-      </DropdownMenuPortal>
-    </DropdownMenuRoot>
+  <!--      <DropdownMenuPortal>-->
+  <!--        <DropdownMenuContent-->
+  <!--            class="bg-white border border-gray-200 shadow-lg py-2 min-w-[200px] text-sm"-->
+  <!--        >-->
+  <!--          <DropdownMenuSub-->
+  <!--              v-for="category in data.categories"-->
+  <!--              :key="category.id"-->
+  <!--          >-->
+  <!--            <DropdownMenuSubTrigger-->
+  <!--                class="px-4 py-2 text-gray-700 cursor-pointer hover:bg-gray-100 transition-colors"-->
+  <!--                @click="test"-->
+  <!--                value="inner1"-->
+  <!--            >-->
+  <!--              {{ category.title }}-->
+  <!--            </DropdownMenuSubTrigger>-->
 
-    <div class="flex-1 h-full flex flex-col items-center justify-center">
-      <h1 class="text-5xl">Kolping Handbuch</h1>
-    </div>
+  <!--            <DropdownMenuPortal>-->
+  <!--              <DropdownMenuSubContent-->
+  <!--                  class="bg-white border border-gray-200 shadow-lg min-w-[200px] text-sm"-->
+  <!--              >-->
+  <!--                <DropdownMenuSub-->
+  <!--                    v-for="sub in category.sub_categories"-->
+  <!--                    :key="sub.id"-->
+  <!--                >-->
+  <!--                  <DropdownMenuSubTrigger-->
+  <!--                      class="px-4 py-2 text-gray-700 cursor-pointer hover:bg-gray-100 transition-colors"-->
+  <!--                      value="inner1"-->
+  <!--                      @click="test"-->
+  <!--                  >-->
+  <!--                    {{ sub.title }}-->
+  <!--                  </DropdownMenuSubTrigger>-->
 
-    <!--    <img class="pl-77" src="/img/header.png" alt="header">-->
-  </div>
+  <!--                  <DropdownMenuPortal>-->
+  <!--                    <DropdownMenuSubContent-->
+  <!--                        class="bg-white border border-gray-200 shadow-lg min-w-[200px] text-sm"-->
+  <!--                    >-->
+  <!--                      <DropdownMenuItem-->
+  <!--                          @select="test"-->
+  <!--                          v-for="topic in sub.topics"-->
+  <!--                          :key="topic"-->
+  <!--                          class="px-4 py-2 text-gray-700 cursor-pointer hover:bg-gray-100 transition-colors"-->
+  <!--                      >-->
+  <!--                        {{ topic }}-->
+  <!--                      </DropdownMenuItem>-->
+  <!--                    </DropdownMenuSubContent>-->
+  <!--                  </DropdownMenuPortal>-->
+  <!--                </DropdownMenuSub>-->
+  <!--              </DropdownMenuSubContent>-->
+  <!--            </DropdownMenuPortal>-->
+  <!--          </DropdownMenuSub>-->
+  <!--        </DropdownMenuContent>-->
+  <!--      </DropdownMenuPortal>-->
+  <!--    </DropdownMenuRoot>-->
+
+  <!--    <div class="flex-1 h-full flex flex-col items-center justify-center">-->
+  <!--      <h1 class="text-5xl">Kolping Handbuch</h1>-->
+  <!--    </div>-->
+
+  <!--    <img class="pl-77" src="/img/header.png" alt="header">-->
+  <!--  </div>-->
 </template>
 
 <style scoped></style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,39 +1,14 @@
 <script setup lang="ts">
 
+import KernprozessCreator from "~/component/kernprozess-creator.vue";
 import TextPage from "~/component/text-page.vue";
 import Kernprozess from "~/component/kernprozess.vue";
-import KernprozessCreator from "~/component/kernprozess-creator.vue";
-
-const colorMode = useColorMode()
-
-const isDark = computed({
-  get() {
-    return colorMode.value === 'dark'
-  },
-  set(_isDark) {
-    colorMode.preference = _isDark ? 'dark' : 'light'
-  }
-})
 
 </script>
 
 <template>
 <!--  <text-page></text-page>-->
-<!--  <kernprozess.md></kernprozess.md>-->
-
-  <ClientOnly v-if="!colorMode?.forced">
-    <UButton
-        :icon="isDark ? 'i-lucide-moon' : 'i-lucide-sun'"
-        color="neutral"
-        variant="ghost"
-        :aria-label="`Switch to ${isDark ? 'light' : 'dark'} mode`"
-        @click="isDark = !isDark"
-    />
-
-    <template #fallback>
-      <div class="size-8" />
-    </template>
-  </ClientOnly>
+<!--  <kernprozess></kernprozess>-->
 
   <kernprozess-creator></kernprozess-creator>
 </template>
