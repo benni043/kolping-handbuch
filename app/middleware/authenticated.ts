1cc70486c434f57a1c432edc4429932346714d77
huff-laptop <benni.huff@gmail.com>
2025-11-12 12:44:00 +0100

    added error pop up

--- app/middleware/authenticated.ts
+++ app/middleware/authenticated.ts
@@ -1,8 +1,15 @@
 export default defineNuxtRouteMiddleware((to) => {
   const { loggedIn, user } = useUserSession();
+  const toast = useToast();
 
   // redirect the user to the login screen if they're not authenticated
   if (!loggedIn.value) {
+    toast.add({
+      title: "Fehler",
+      description: "Sie müssen angemeldet sein, um diesen Inhalt zu sehen!",
+      color: "error",
+      icon: "i-heroicons-x-mark",
+    });
     return navigateTo(`/kolping/${loginLower}`);
   }
 
