ed28dc33ffe0946ac9592e49b822beef65757062
huff-laptop <benni.huff@gmail.com>
2025-12-19 11:38:25 +0100

    fixed deleting

--- server/api/files/.delete.ts
+++ server/api/files/.delete.ts
@@ -1,4 +1,4 @@
-import { unlink } from "fs/promises";
+import { unlink, rm, stat } from "fs/promises";
 import { join } from "path";
 
 export default defineEventHandler(async (event) => {
@@ -9,12 +9,16 @@ export default defineEventHandler(async (event) => {
 
   const body = await readBody<{
     path: string;
+    item: string;
   }>(event);
 
   if (!body.path)
     throw createError({ statusCode: 400, statusMessage: "Invalid request" });
 
-  const filePath = join(process.cwd(), `data/files/${body.path}`);
+  const filePath = join(process.cwd(), `data/files/${body.path}/${body.item}`);
 
-  await unlink(filePath);
+  const s = await stat(filePath);
+
+  if (s.isDirectory()) await rm(filePath, { recursive: true, force: true });
+  else await unlink(filePath);
 });
