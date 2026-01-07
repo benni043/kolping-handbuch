75b337e074e5cda4c744598495a8a3363d55fa03
huff-laptop <benni.huff@gmail.com>
2026-01-07 10:11:56 +0100

    added check if new name already exists

--- server/api/files/folder.put.ts
+++ server/api/files/folder.put.ts
@@ -1,4 +1,4 @@
-import { rename } from "fs/promises";
+import { rename, stat } from "fs/promises";
 import { join } from "path";
 
 export default defineEventHandler(async (event) => {
@@ -13,13 +13,23 @@ export default defineEventHandler(async (event) => {
     newName: string;
   }>(event);
 
-  if (!body.path || !body.newName)
+  if (!body.path || !body.oldName || !body.newName)
     throw createError({ statusCode: 400, statusMessage: "Invalid request" });
 
   const base = join(process.cwd(), "data/files");
-
   const oldPath = join(base, body.path, body.oldName);
   const newPath = join(base, body.path, body.newName);
+
+  try {
+    await stat(newPath);
+
+    throw createError({
+      statusCode: 409,
+      statusMessage: "File already exists",
+    });
+  } catch (err: any) {
+    if (err.code !== "ENOENT") throw err;
+  }
 
   await rename(oldPath, newPath);
 });
