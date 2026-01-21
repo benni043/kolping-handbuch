75ba2cb38ab5eb7c3e1d525d5d7758d0398754fc
huff-laptop <benni.huff@gmail.com>
2026-01-21 11:23:44 +0100

    height fix

--- app/components/nav/NavMenuElement.vue
+++ app/components/nav/NavMenuElement.vue
@@ -531,7 +531,7 @@ async function rename(newName: string) {
     <ul :class="{ hidden: !(width <= MOBILE_WIDTH) }">
       <li v-for="(path, index) in data" :key="path.id">
         <div
-          class="flex items-center h-13 bg-[#50A9CE]/[0.33] border-b-1 border-b-gray-400"
+          class="flex items-center min-h-13 bg-[#50A9CE]/[0.33] border-b-1 border-b-gray-400"
           @click="
             hoveredCategory === null || hoveredCategory !== index
               ? enterCategory(index)
@@ -629,7 +629,7 @@ async function rename(newName: string) {
         <ul v-if="hoveredCategory === index">
           <li v-for="(subPath, subIndex) in path.children" :key="subPath.id">
             <div
-              class="flex items-center h-13 bg-[#50A9CE]/[0.33] min-w-88 border-b-1 border-b-gray-400"
+              class="flex items-center min-h-13 bg-[#50A9CE]/[0.33] min-w-88 border-b-1 border-b-gray-400"
               @click="
                 hoveredSub === null || hoveredSub !== subIndex
                   ? enterSub(subIndex)
