020dcbd69d898b7d7b8a70b4944c5326331469a4
huff-laptop <benni.huff@gmail.com>
2026-01-27 14:57:56 +0100

    added instructions

--- server/utils/traversal.ts
+++ server/utils/traversal.ts
@@ -2,8 +2,9 @@ import { resolve } from "path";
 
 export const CONTENT_ROOT = resolve(process.cwd(), "data/content");
 export const FILE_ROOT = resolve(process.cwd(), "data/files");
-export const PUBLIC_ROOT = resolve(process.cwd(), "data");
 export const METADATA_ROOT = resolve(process.cwd(), "data/metadata");
+export const DOKU_ROOT = resolve(process.cwd(), "data/doku");
+export const PUBLIC_ROOT = resolve(process.cwd(), "data");
 
 export function safeJoin(root: string, unsafePath: string) {
   const targetPath = resolve(root, unsafePath);
